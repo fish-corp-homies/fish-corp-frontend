70fc810ba4ed01db33a2ae28975f8d4b7bbfa9a2
Stian Munkejord <smu027@uib.no>
2026-03-24 21:15:17 +0100

    Centered the navbar

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -2,18 +2,18 @@ import Link from "next/link";
 
 export default function Navbar() {
     return (
-        <nav>
-                <ul>
-                    <li>
-                        <Link href="/">Home</Link>
-                    </li>
-                    <li>
-                        <Link href="/about">About</Link>
-                    </li>
-                    <li>
-                        <Link href="/contact">Contact</Link>
-                    </li>
-                </ul>
+        <nav className="flex justify-center">
+            <ul className="flex gap-4">
+                <li>
+                    <Link href="/">Home</Link>
+                </li>
+                <li>
+                    <Link href="/about">About</Link>
+                </li>
+                <li>
+                    <Link href="/contact">Contact</Link>
+                </li>
+            </ul>
         </nav>
     );
 }
