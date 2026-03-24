aa5f94f73d6fa522956848bb96cc00f00bc78d77
Stian Munkejord <smu027@uib.no>
2026-03-24 22:12:52 +0100

    Button to 404 page

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,7 +1,13 @@
+'use client';
+
+import Link from "next/link";
+
 export default function Home() {
-  return (
-      <>
-          <h1>Home</h1>
-      </>
-  );
+
+    return (
+        <>
+            <h1>Home</h1>
+            <Link href="/404">To the 404 page</Link>
+        </>
+    );
 }
