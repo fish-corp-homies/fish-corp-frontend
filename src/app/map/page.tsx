10bd4bdcee6e22a2fddc31f9f4aa5c51789dccc8
Stian Munkejord <smu027@uib.no>
2026-03-25 20:10:12 +0100

    Pyntet på skjemaet

--- src/app/map/page.tsx
+++ src/app/map/page.tsx
@@ -1,12 +1,31 @@
-export default function Map() {
-    return(
+export default function Home() {
+    return (
         <>
-            <h1>Norges kart insert here</h1>
-            <form>
-                <input type="text" placeholder="Lengdegrad" />
-                <input type="text" placeholder="Breddegrad" />
-                <button type="submit">Insert here</button>
-            </form>
+            <div className="text-center bg-gray-400">
+                <h1 className="text-xl font-semibold">Cords</h1>
+
+                <form className="flex flex-col gap-3 mt-5">
+                    <p>Breddegrad:</p>
+                    <input
+                        type="text"
+                        placeholder="60.1"
+                        className="px-3 py-2 rounded border"
+                    />
+
+                    <p>Lengdegrad:</p>
+                    <input
+                        type="text"
+                        placeholder="5"
+                        className="px-3 py-2 rounded border"
+                    />
+                    <button
+                        type="submit"
+                        className="px-4 py-2 bg-black text-white rounded"
+                    >
+                        Insert here
+                    </button>
+                </form>
+            </div>
         </>
-    )
+    );
 }
