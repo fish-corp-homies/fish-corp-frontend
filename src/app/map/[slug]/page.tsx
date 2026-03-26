9fb46ec0b6deb5eca005d8bfa45f52a789589d93
Stian Munkejord <smu027@uib.no>
2026-03-26 20:34:30 +0100

    Static page

--- src/app/map/[slug]/page.tsx
+++ src/app/map/[slug]/page.tsx
@@ -1,16 +1,7 @@
-'use client'
-import {use} from 'react'
-
-export default function LocationPage({
-    params,
-}: {
-    params: Promise<{ slug: string }>
-}) {
-    const {slug} = use(params)
-
+export default function LocationPage({params}: { params: { slug: string };}) {
     return (
         <div>
-            <p>{slug}</p>
+            <p>{params.slug}</p>
         </div>
     )
 }
